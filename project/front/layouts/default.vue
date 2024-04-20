31914d6fcd55f966da6d5ee02d3e75bfc4650fb9
mau vargas <linuxlite20@gmail.com>
2024-04-20 16:54:29 +0200

    cv complete

--- project/front/layouts/default.vue
+++ project/front/layouts/default.vue
@@ -1,30 +1,18 @@
 <template>
   <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap"
-      rel="stylesheet"
-    />
-    <!-- <script
-      src="https://kit.fontawesome.com/bfa0a87a28.js"
-      crossorigin="anonymous"
-    ></script> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kode+Mono&display=swap" rel="stylesheet">
   </Head>
   <div>
-    <!-- <Navbar /> -->
-
-    <!-- <main class="container p-3 my-5 border rounded shadow"> -->
     <slot></slot>
-    <!-- </main> -->
-
     <Footer />
   </div>
 </template>
 
 <script setup>
 useHead({
-  titleTemplate: '%s - Intro Nuxt',
+  titleTemplate: 'Mauricio Vargas',
   script: [
     {
       src: 'https://kit.fontawesome.com/bfa0a87a28.js',
