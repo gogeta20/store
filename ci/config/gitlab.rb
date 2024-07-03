0f7698a749f5a2f276f3585f44b5af1f43501943
Cesar <codigomau@gmail.com>
2024-07-03 14:40:21 +0200

    feat: config

--- ci/config/gitlab.rb
+++ ci/config/gitlab.rb
@@ -3,3 +3,5 @@ nginx['enable'] = true
 nginx['redirect_http_to_https'] = true
 nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.crt"
 nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.key"
+gitlab_workhorse['listen_network'] = "unix"
+gitlab_workhorse['listen_addr'] = "/var/opt/gitlab/gitlab-workhorse/socket"
