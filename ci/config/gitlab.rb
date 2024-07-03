9dca3ccde038bee26f9afa76f97129962a81dcf7
Cesar <codigomau@gmail.com>
2024-07-03 11:50:53 +0200

    feat: config nueva para el certificado

--- ci/config/gitlab.rb
+++ ci/config/gitlab.rb
@@ -1,4 +1,5 @@
 external_url "https://gitlab.mvm"
+nginx['enable'] = true
 nginx['redirect_http_to_https'] = true
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.mvm.crt"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.mvm.key"
+nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.crt"
+nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.key"
